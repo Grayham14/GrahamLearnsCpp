96aaa17763eaec2ef50570cc8efd7bf153e1f771
Graham Tuckfield-Porro <grahamtuckfieldporro@gmail.com>
2026-09-15 11:36:16 -0700

    cat cat cat cat

--- cIn.cpp
+++ cIn.cpp
@@ -10,6 +10,7 @@ int main() {
     std::cin >> name;
     std::cout << "Enter your cats age: ";
     std::cin >> age;
+    std::cout << "Here are your cats details: " << std::endl;
 
     std::cout << std::format("Your cat's name is {} and it is {} years old.", name, age) << std::endl;
 }
