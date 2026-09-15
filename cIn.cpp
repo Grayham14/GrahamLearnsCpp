85d7ec10878343c0f1d91fb92822903f339872c4
Graham Tuckfield-Porro <grahamtuckfieldporro@gmail.com>
2026-09-15 11:31:52 -0700

    added in cat, i need to learn what a git.ignore is lwky

---
+++ cIn.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <format>
+
+int main() {
+    std::string name; 
+    int age;
+
+    std::cout << "Enter your cats name: ";
+    std::cin >> name;
+    std::cout << "Enter your cats age: ";
+    std::cin >> age;
+
+    std::cout << std::format("Your cat's name is {} and it is {} years old.", name, age) << std::endl;
+}
+
