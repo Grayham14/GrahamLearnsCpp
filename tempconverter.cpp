971aabc70db817d11e9ab1bb2f7e744d51e09f97
Graham Tuckfield-Porro <grahamtuckfieldporro@gmail.com>
2026-09-16 14:28:32 -0700

    added different read inputs for intagers and strings, and added in error messages as method peramiters

--- tempconverter.cpp
+++ tempconverter.cpp
@@ -4,7 +4,29 @@
 
 //temprature converter that reads C or F and converts to C or F respectivily
 
-int ReadInput(const std::string prompt){
+std::string ReadInputString(const std::string prompt, const std::string errorMsg){
+    while(true)
+    {
+        std::cout << prompt;
+        std::string value{};
+        //make sure only numbers get put in
+        if(std::cin >> value)
+        {
+            return value;
+        }
+
+        if (std::cin.eof())
+        {
+            std::exit(0);   // input was closed entirely (Ctrl+Z on Windows)
+        }
+
+        std::cout << errorMsg << "\n";
+        std::cin.clear();   // reset the failure state
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');   // discard the bad line
+    }
+}
+
+int ReadInputInt(const std::string prompt, const std::string errorMsg){
     while(true)
     {
         std::cout << prompt;
@@ -20,13 +42,16 @@ int ReadInput(const std::string prompt){
             std::exit(0);   // input was closed entirely (Ctrl+Z on Windows)
         }
 
-        std::cout << "That's not a whole number. Try again.\n";
+        std::cout << errorMsg << "\n";
         std::cin.clear();   // reset the failure state
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');   // discard the bad line
     }
 }
 
 
+
+
 int main(){
-    ReadInput("give me a number please");
+    std::string unit = ReadInputString("Enter C for Celsius or F for Fahrenheit: ");
+
 }
